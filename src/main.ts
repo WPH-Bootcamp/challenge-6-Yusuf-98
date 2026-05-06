529c091fb8bb08937418a64a454f51790f36e3f9
Yusuf-98 <yusuf.smg@gmail.com>
2026-05-07 02:45:17 +0700

    add: file gitignore dan testing aplikasi

--- src/main.ts
+++ src/main.ts
@@ -12,3 +12,40 @@ console.log("=====================================");
 
 // Mulai pengujian di bawah ini
 
+import { Book } from "./types";
+import { books } from "./data/books";
+import { addBook, listBooks, searchBook } from "./functions/bookManager";
+
+const book1: Book = {
+  title: "Laskar Pelangi",
+  author: "Andrea Hirata",
+  publicationYear: 2005
+};
+
+const book2: Book = {
+  title: "Clean Code",
+  author: "Robert C. Martin",
+  publicationYear: 2008
+};
+
+const book3: Book = {
+  title: "Madilog",
+  author: "Tan Malaka",
+  publicationYear: 1951
+};
+
+// --- Test Aplikasi --- //
+
+addBook(book1);
+addBook(book2);
+addBook(book3);
+
+console.log(books)
+
+listBooks();
+
+searchBook('clean');
+
+searchBook('harry');
+
+searchBook();
